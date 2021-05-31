8eee525c2434a8ee3112775ab8a888e1f1ffc605
liuxiaozhong <>
2021-05-31 11:31:40 +0800

    update version 4.2.1.1

--- AR-Android-Tutorial-1to1/sample/app/src/main/java/org/ar/ar_android_tutorial_1to1/VideoActivity.java
+++ AR-Android-Tutorial-1to1/sample/app/src/main/java/org/ar/ar_android_tutorial_1to1/VideoActivity.java
@@ -197,6 +197,7 @@ public class VideoActivity extends AppCompatActivity implements View.OnClickList
         if (rlLocal!=null){
             rlLocal.removeAllViews();
         }
+
     }
     private void startCall() {
         setupLocalVideo();
